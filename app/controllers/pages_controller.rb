78b47c362073ceb9739d361a93edc86c61574bdb
axel burtz <axelburtz@gmail.com>
2021-08-24 18:46:51 +0200

    method reco author and type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,6 @@ class PagesController < ApplicationController
 
   def dashboard
     @all_artworks_liked_id = current_user.all_favorites.pluck(:favoritable_id)
-    @all_artworks_liked = Artwork.where(id: @all_artworks_liked_id)
-    @doublon = Hash.new(0).tap { |h| @all_artworks_liked.each { |v| h[v] +=1 } }
+    @all_artworks_liked =  Artwork.where(id: @all_artworks_liked_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :user_exhibitions
   has_many :reviews
   acts_as_favoritor
+
+  def recomandations_author
+    Artwork.joins(:author).where(id: all_favorites.pluck(:favoritable_id)).group('authors.id').count
+  end
+
+  def recomandations_type
+    Artwork.joins(:type).where(id: all_favorites.pluck(:favoritable_id)).group('type.id').count
+  end
+
+  def recomandations_museum
+    Artwork.where(id: all_favorites.pluck(:favoritable_id)).group('museum').count
+  end
 end

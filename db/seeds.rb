2487fef872beb2c3afcf36d073633dde430efe1a
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-24 12:42:14 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ Exhibition.delete_all
 # Reviews.delete_all
 
 puts "creating user"
-user1 = User.create(email: 'axel@wanadoo.com', password: "1234567")
+user1 = User.create(email: 'axel@gmail.com', password: "1234567")
 user1.save!
-user2 = User.new(email: 'tristan@hotmail.com', password: "1234567")
+user2 = User.new(email: 'tristan@gmail.com', password: "1234567")
 user2.save!
 user3 = User.new(email: 'jean@gmail.com', password: "1234567")
 user3.save!

462585ea033de691af5af28911aee80729fb9c46
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-30 15:25:53 +0200

    did the artwork show

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :artworks, only: %i[like dislike]
+  resources :exhibitions, only: %i[show]
   resources :artworks, only: %i[create new edit update] do
-    member do 
+    member do
       post 'like'
       post 'dislike'
     end
   end
-
   get 'dashboard', to: 'pages#dashboard'
 end

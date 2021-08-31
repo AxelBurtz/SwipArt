3ee70995633e0b034c4a00935be85a692bbceafc
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2021-08-31 18:27:35 +0200

    Fix the issue with JS and jquery star form

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,5 +1,5 @@
 import "jquery-bar-rating";
-import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
+// import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
   $('#review_rating').barrating({

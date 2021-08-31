b9d2a5615db1339965ba4acf13b2c93636e53f07
axel burtz <axelburtz@gmail.com>
2021-08-31 09:23:54 +0200

    modif user_exhibitions controller

--- app/controllers/user_exhibitions_controller.rb
+++ app/controllers/user_exhibitions_controller.rb
@@ -26,27 +26,7 @@ class UserExhibitionsController < ApplicationController
     # end
     #refacto in one line
     UserExhibition.find_or_create_by(user: current_user, exhibition: @exhibition).update(status: "discarded")
-    redirect_to dashboard_path
-  end
-
-  def new_booking
-    @exhibition = Exhibition.find(params[:exhibition_id])
-    @user_exhibition = UserExhibition.new_booking
-  end 
-  
-  def create_booking
-    @exhibition = Exhibition.find(params[:exhibition_id])
-    if user_exhibition.nil?
-    UserExhibition.find_by(user: current_user, exhibition: @exhibition).update(status: "booked")
-    else
-      UserExhibition.create(user: current_user, exhibition: @exhibition, status: "booked")
-    end 
-    # @et_inchallah_ca_marche
-    # redirect_to user_exhibitions_saved
-  end  
-
-  def booking
-    redirect_to dashboard_path    
+  redirect_to dashboard_path
   end
 
 private
